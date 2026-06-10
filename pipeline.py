c3fabfdb69fe545a75ab4512f9e98892d431afee
fhmyazz <fahmyaziz1805@gmail.com>
2026-06-10 21:34:37 +0700

    add error handling and logging

--- pipeline.py
+++ pipeline.py
@@ -1,14 +1,13 @@
 import requests
 import pandas as pd
-from sqlalchemy import create_engine
 from dotenv import load_dotenv
 from datetime import datetime
 import os
 import time
+import logging
 
 from google.cloud import bigquery
 from google.oauth2 import service_account
-import pandas_gbq
 
 load_dotenv()
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
@@ -22,172 +21,227 @@ HEADERS = {
 
 SEARCH_URL = "https://api.github.com/search/repositories"
 
-print("[INFO]: Setup is completed...")
+BIGQUERY_KEY_PATH = "./inlaid-particle-359102-118bdae159e1.json"
+BIGQUERY_PROJECT_ID = "inlaid-particle-359102"
+BIGQUERY_DATASET = "github_data"
+BIGQUERY_TABLE = "repositories"
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+    handlers=[
+        logging.FileHandler('pipeline.log'),
+        logging.StreamHandler()
+    ]
+)
+logger = logging.getLogger(__name__)
+
+logger.info("[INFO]: Setup is completed...")
 def extract(language="python", min_stars=1000, max_pages=2, per_page=10):
-    print("[INFO]: Starting Extraction...")
     """
         Extract repos from Github API
         Return list of repo dictionaries
     """
-    repos = []
+    logger.info("[INFO]: Starting Extraction...")
 
-    params = {
-        "q": f"language:{language} stars:>{min_stars}",
-        "sort": "stars",
-        "order": "desc",
-        "per_page": per_page
-    }
+    try:
+        repos = []
 
-    for page in range (1, max_pages+1):
-        params["page"] = page
-        print(f"[INFO]: Fetching page: {page}")
+        params = {
+            "q": f"language:{language} stars:>{min_stars}",
+            "sort": "stars",
+            "order": "desc",
+            "per_page": per_page
+        }
 
-        response = requests.get(SEARCH_URL, headers=HEADERS, params=params)
+        for page in range (1, max_pages+1):
+            params["page"] = page
+            print(f"[INFO]: Fetching page: {page}")
 
-        remaining = int(response.headers.get("X-RateLimit-Remaining", 0))
-        print(f"[INFO]: Rate limit remaining: {remaining}")
+            response = requests.get(SEARCH_URL, headers=HEADERS, params=params)
+            response.raise_for_status()
 
-        if response.status_code != 200:
-            print(f"[ERROR]: {response.status_code} {response.text}")
-            break
+            remaining = int(response.headers.get("X-RateLimit-Remaining", 0))
+            print(f"[INFO]: Rate limit remaining: {remaining}")
+
+            if response.status_code != 200:
+                print(f"[ERROR]: {response.status_code} {response.text}")
+                break
+            
+            data = response.json()
+            items = data.get("items", [])
+            repos.extend(items)
+
+            if len(items) == 0:
+                break
+
+            time.sleep(1)
         
-        data = response.json()
-        items = data.get("items", [])
-        repos.extend(items)
+        print(f"[INFO]: Extracted {len(repos)} repos")
+        return repos
+    
+    except requests.exceptions.RequestException as e:
+        logger.error(f"API request failed: {e}")
+        return []
 
-        if len(items) == 0:
-            break
-
-        time.sleep(1)
-       
-    print(f"[INFO]: Extracted {len(repos)} repos")
-    return repos
+    except Exception as e:
+        logger.exception(f"Unexpected error in extract(): {e}")
+        return []
 
 def transform(repos):
     """
     Transform raw API into clean DataFrame
     Returns DataFrame
     """
+    try:
+        if not repos:
+            logger.info("[INFO]: No data to transform")
+            return pd.DataFrame()
 
-    if not repos:
-        print("[INFO]: No data to transform")
-        return pd.DataFrame()
+        # Buat DataFrame dari list repos
+        df = pd.DataFrame([{
+            "id": r["id"],
+            "name": r["name"],
+            "full_name": r["full_name"],
+            "owner": r["owner"]["login"],
+            "language": r["language"],
+            "stars": r["stargazers_count"],
+            "forks": r["forks_count"],
+            "open_issues": r["open_issues_count"],
+            "description": r["description"],
+            "created_at": pd.to_datetime(r["created_at"]),
+            "updated_at": pd.to_datetime(r["updated_at"]),
+            "url": r["html_url"]
+        } for r in repos])
 
-         # Buat DataFrame dari list repos
-    df = pd.DataFrame([{
-        "id": r["id"],
-        "name": r["name"],
-        "full_name": r["full_name"],
-        "owner": r["owner"]["login"],
-        "language": r["language"],
-        "stars": r["stargazers_count"],
-        "forks": r["forks_count"],
-        "open_issues": r["open_issues_count"],
-        "description": r["description"],
-        "created_at": pd.to_datetime(r["created_at"]),
-        "updated_at": pd.to_datetime(r["updated_at"]),
-        "url": r["html_url"]
-    } for r in repos])
+        initial_count = len(df)
+        df = df.drop_duplicates(subset="id")
+        logger.info(f"[INFO]: Removed {initial_count - len(df)} duplicate id")
 
-    initial_count = len(df)
-    df = df.drop_duplicates(subset="id")
-    print(f"[INFO]: Removed {initial_count - len(df)} duplicate id")
+        df["description"] = df["description"].fillna("No Description")
+        df["fetched_at"] = datetime.now()
 
-    df["description"] = df["description"].fillna("No Description")
-    df["fetched_at"] = datetime.now()
+        def stars_category(stars):
+            if stars >= 100000:
+                return "legendary"
+            elif stars >= 10000:
+                return "popular"
+            else:
+                return "rising"
 
-    def stars_category(stars):
-        if stars >= 100000:
-            return "legendary"
-        elif stars >= 10000:
-            return "popular"
-        else:
-            return "rising"
+        df["stars_category"] = df["stars"].apply(stars_category)
+        df = df.sort_values("stars", ascending=False)
 
-    df["stars_category"] = df["stars"].apply(stars_category)
-    df = df.sort_values("stars", ascending=False)
-
-    print(f"Transform completed. {len(df)} rows, {len(df.columns)} columns")
-    return df
-
-def load(df, db_path="data/repos.db", table_name="repositories"):
-    """
-    Load DataFrame into SQLite Database
-    """
-
-    if df.empty:
-        print("[INFO]: No Data to Load")
-        return
+        logger.info(f"Transform completed. {len(df)} rows, {len(df.columns)} columns")
+        return df
     
-    os.makedirs(os.path.dirname(db_path), exist_ok=True)
+    except Exception as e:
+        logger.exception(f"Unexpected error in transform(): {e}")
 
-    engine = create_engine(f"sqlite:///{db_path}")
+# def load(df, db_path="data/repos.db", table_name="repositories"):
+#     """
+#     Load DataFrame into SQLite Database
+#     """
 
-    df.to_sql(table_name, engine, if_exists="replace", index=False)
+#     if df.empty:
+#         print("[INFO]: No Data to Load")
+#         return
+    
+#     os.makedirs(os.path.dirname(db_path), exist_ok=True)
 
-    print(f"[INFO]: Loaded {len(df)} rows to db {db_path}, table: {table_name}")
+#     engine = create_engine(f"sqlite:///{db_path}")
 
-def test_bigq_connection():
-    """
-    Test connection to BigQuery by querying public dataset
-    """
+#     df.to_sql(table_name, engine, if_exists="replace", index=False)
 
-    # credentials from bigquery
-    key_path = "./inlaid-particle-359102-118bdae159e1.json"
+#     print(f"[INFO]: Loaded {len(df)} rows to db {db_path}, table: {table_name}")
 
-    # create credentials from the json
+def get_bigq_client():
     credentials = service_account.Credentials.from_service_account_file(
-        key_path,
-        scopes = ["https://www.googleapis.com/auth/cloud-platform"],
+        BIGQUERY_KEY_PATH,
+        scopes=["https://www.googleapis.com/auth/cloud-platform"]
     )
+    return bigquery.Client(credentials=credentials, project=BIGQUERY_PROJECT_ID)
 
-    # create object client
-    client = bigquery.Client(credentials=credentials, project=credentials.project_id)
 
-    # simple query to bigq dataset
-    query = """
-        SELECT name, sum(number) as total
-        FROM `bigquery-public-data.usa_names.usa_1910_current`
-        WHERE state = 'TX'
-        GROUP BY name
-        ORDER BY total DESC
-        LIMIT 5
-    """
+# def test_bigq_connection():
+#     """
+#     Test connection to BigQuery by querying public dataset
+#     """
+#     client = get_bigq_client()
 
-    print("[INFO]: Running query from bigquery")
+#     # # credentials from bigquery
+#     # key_path = "./inlaid-particle-359102-118bdae159e1.json"
 
-    # running query and convert into pandas DF
-    df_test = client.query(query).to_dataframe()
+#     # # create credentials from the json
+#     # credentials = service_account.Credentials.from_service_account_file(
+#     #     key_path,
+#     #     scopes = ["https://www.googleapis.com/auth/cloud-platform"],
+#     # )
 
-    print("Connection Sucessful! Query Result: ")
-    print(df_test)
-    return True
+#     # # create object client
+#     # client = bigquery.Client(credentials=credentials, project=credentials.project_id)
 
-def load_to_bigq(df, dataset_id="github_data", table_id="repositories"):
+#     # simple query to bigq dataset
+#     query = """
+#         SELECT name, sum(number) as total
+#         FROM `bigquery-public-data.usa_names.usa_1910_current`
+#         WHERE state = 'TX'
+#         GROUP BY name
+#         ORDER BY total DESC
+#         LIMIT 5
+#     """
+
+#     print("[INFO]: Running query from bigquery")
+
+#     # running query and convert into pandas DF
+#     df_test = client.query(query).to_dataframe()
+
+#     print("Connection Sucessful! Query Result: ")
+#     print(df_test)
+#     return True
+
+def load_to_bigq(df):
     """
     Load to DataFrame BigQuery
     """
-    if df.empty:
-        print("No data to load")
-        return
+    try:
+        if df.empty:
+            logger.info("No data to load")
+            return
 
-    key_path = "./inlaid-particle-359102-118bdae159e1.json"
-    project_id = "inlaid-particle-359102"
-    table_ref = f"{project_id}.{dataset_id}.{table_id}"
+        # key_path = "./inlaid-particle-359102-118bdae159e1.json"
+        # project_id = "inlaid-particle-359102"
+        # table_ref = f"{project_id}.{dataset_id}.{table_id}"
 
-    credentials = service_account.Credentials.from_service_account_file(key_path)
+        client = get_bigq_client()
 
-    print(f"[INFO]: Load {len(df)} rows into {table_ref}")
-    df.to_gbq(
-        destination_table = table_ref,
-        project_id = project_id,
-        credentials = credentials,
-        if_exists = "replace",
-        progress_bar = True
-    )
+        dataset_id = f"{BIGQUERY_PROJECT_ID}.{BIGQUERY_DATASET}"
+        table_id = f"{dataset_id}.{BIGQUERY_TABLE}"
 
-    print(f"[INFO]: Data successfully loaded to BigQuery!")
+        dataset = bigquery.Dataset(dataset_id)
+        dataset.location = "asia-southeast2"  # Jakarta
+        client.create_dataset(dataset, exists_ok=True)
+
+        job_config = bigquery.LoadJobConfig(
+            write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE
+        )
+
+        job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
+        job.result()
+
+        logger.info(f"[INFO]: Load {len(df)} rows into {table_id}")
+        # df.to_gbq(
+        #     destination_table = table_ref,
+        #     project_id = project_id,
+        #     credentials = credentials,
+        #     if_exists = "replace",
+        #     progress_bar = True
+        # )
+
+        logger.info(f"[INFO]: Data successfully loaded to BigQuery!")
+
+    except Exception as e:
+        logger.exception(f"Unexpected error in transform(): {e}")
     
 
 if __name__ == "__main__":
@@ -197,5 +251,5 @@ if __name__ == "__main__":
     load_to_bigq(df)
     # # print(df.head())
     # load(df)
-    print("[INFO]: Pipeline completed successfully")
+    logger.info("[INFO]: Pipeline completed successfully")
     # test_bigq_connection()
